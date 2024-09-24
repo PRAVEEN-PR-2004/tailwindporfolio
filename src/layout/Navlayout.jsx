d33e2a48296c10d7c8100b74c9267a2396ff0d5f
PRAVEEN-PR-2004 <praveen2004ttp@gmail.com>
2024-09-24 15:07:22 +0530

    checking purpose

--- src/layout/Navlayout.jsx
+++ src/layout/Navlayout.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import Navbar from "../components/Navbar";
+import { Outlet } from "react-router-dom";
 
 export const Navlayout = () => {
   return (
     <>
-    <Navbar/>
-    <Outlet/>
+      {/* <Navbar/> */}
+      <Outlet />
     </>
-  )
-}
+  );
+};
