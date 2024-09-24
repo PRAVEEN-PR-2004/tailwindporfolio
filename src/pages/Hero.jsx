fc748a36e841fa31106f4a46fd883e9cfb318420
PRAVEEN-PR-2004 <praveen2004ttp@gmail.com>
2024-09-24 15:05:08 +0530

    check purpose

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,46 +1,48 @@
-import { TbBrandGithubFilled } from 'react-icons/tb';
-import { FaLinkedinIn } from 'react-icons/fa';
-import { SiLeetcode } from 'react-icons/si';
-import ParticlesBg from 'particles-bg'
+import { TbBrandGithubFilled } from "react-icons/tb";
+import { FaLinkedinIn } from "react-icons/fa";
+import { SiLeetcode } from "react-icons/si";
+import ParticlesBg from "particles-bg";
 import Skills from "../components/Skills";
 import Footer from "../components/Footer";
 import Projects from "../components/Projects";
-import About from './About';
+import About from "./About";
 const Hero = () => {
   return (
     <>
-   
-    <div className="flex flex-col items-center justify-center h-screen px-4 ">
-      <h1 className="mb-6 text-4xl font-bold text-center text-white md:mb-9 md:text-7xl">
-        Hello,<br></br> I'm PRAVEEN PR
-      </h1>
-      <p className="items-center max-w-2xl mb-8 text-sm text-center text-gray-300 md:text-xl">
-        A dedicated and enthusiastic computer science student actively working towards a bachelor's degree. Beyond technical prowess, I bring a multifaceted approach to my work with a penchant for project management.
-      </p>
-      
-      <div className="flex space-x-6">
-        {/* GitHub Icon */}
-        <a href="#" className="text-3xl hover:text-blue-900">
-          <TbBrandGithubFilled size={40} />
-        </a>
-        
-        {/* LinkedIn Icon */}
-        <a href="#" className="text-3xl hover:text-blue-500">
-          <FaLinkedinIn size={40} />
-        </a>
-        
-        {/* LeetCode Icon */}
-        <a href="#" className="text-3xl hover:text-orange-600">
-          <SiLeetcode size={40} />
-        </a>
+      <div className="flex flex-col items-center justify-center h-screen px-4 ">
+        <h1 className="mb-6 text-4xl font-bold text-center text-white md:mb-9 md:text-7xl">
+          Hello,<br></br> I'm PRAVEEN PR
+        </h1>
+        <p className="items-center max-w-2xl mb-8 text-sm text-center text-gray-300 md:text-xl">
+          A dedicated and enthusiastic computer science student actively working
+          towards a bachelor's degree. Beyond technical prowess, I bring a
+          multifaceted approach to my work with a penchant for project
+          management.
+        </p>
+
+        <div className="flex space-x-6">
+          {/* GitHub Icon */}
+          <a href="#" className="text-3xl hover:text-blue-900">
+            <TbBrandGithubFilled size={40} />
+          </a>
+
+          {/* LinkedIn Icon */}
+          <a href="#" className="text-3xl hover:text-blue-500">
+            <FaLinkedinIn size={40} />
+          </a>
+
+          {/* LeetCode Icon */}
+          <a href="#" className="text-3xl hover:text-orange-600">
+            <SiLeetcode size={40} />
+          </a>
+        </div>
       </div>
-    </div>
-     <ParticlesBg type="circle" bg={true} />
-     <About/>
+      <ParticlesBg type="circle" bg={true} />
+      {/* <About/>
      <Skills/>
      <Projects/>
-     <Footer/>
-     </>
+     <Footer/> */}
+    </>
   );
 };
 
